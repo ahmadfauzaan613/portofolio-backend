f991c695f008fecf4e3105f8daa8201eee27090d
ahmadfauzaan613 <ahmadfauzaan613@gmail.com>
2026-02-09 20:24:56 +0700

    fixing db connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,11 +4,11 @@ import { Pool } from 'pg'
 dotenv.config()
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_DB,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
 })
 
 pool.on('connect', () => {
